d8f17baf3a1cb13614eb073c08ca25ae39d3e226
Suraj Jha <sjhadeveloper@gmail.com>
2026-05-27 18:22:40 +0530

    Add files via upload

---
+++ lec-7-npm/app.js
@@ -0,0 +1,13 @@
+// Simple nodeJS Server
+
+const http = require("http");
+
+
+const server = http.createServer((req,res)=>{
+  console.log(req);
+});
+
+const PORT = 3000;
+server.listen(PORT,()=>{
+    console.log("Server running on port "+" "+'http://localhost:'+PORT)
+});
